a0d88e1ec12c579ce084521190445e2ac0421ccd
SARAWIN_B <sarawin.14156@gmail.com>
2023-09-26 14:53:01 +0700

    Action Button

--- Tkinter_Project/main.py
+++ Tkinter_Project/main.py
@@ -1,9 +1,8 @@
 from tkinter import *
-from tkinter import ttk 
-
+from tkinter.ttk import *
 # Create Oject 
 root = Tk()
-
+style = Style()
 
 
 
@@ -14,6 +13,24 @@ root.geometry('1000x500')
 root.title('GUI')
 
 # Set icon  
-root.iconbitmap(r'D:\python_proj\image\python_button_icon_151925.ico')
+# root.iconbitmap(r'D:\python_proj\image\python_button_icon_151925.ico')
+
+# Style Button 
+style.configure('W.TButton1', font=
+                ('calibri',10,'bold','underline'),
+                foreground='red'
+                )
+
+def ActionButton():
+    
+    return 
+
+btn1 = Button(root, text="Click me", style="W.TButton")
+btn1.grid(row=0, column=3, padx=100)
+# btn1.pack(side='left')
+
+btn2 = Button(text="Ok!", style="W.TButton")
+btn2.grid(row=1, column=3, padx=100)
+# btn2.pack(side='right')
 
 root.mainloop() 
